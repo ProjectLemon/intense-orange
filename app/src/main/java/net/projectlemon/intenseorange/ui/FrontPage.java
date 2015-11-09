8f0d9199c7d78de4a2bab1c99235be13224e4741
Fredrik Johansson <f95johansson@gmail.com>
2015-11-09 15:50:32 +0100

    Actually builds now

--- app/src/main/java/net/projectlemon/intenseorange/ui/FrontPage.java
+++ app/src/main/java/net/projectlemon/intenseorange/ui/FrontPage.java
@@ -1,4 +1,4 @@
-package net.projectlemon.intenseorange;
+package net.projectlemon.intenseorange.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import net.projectlemon.intenseorange.R;
 
 public class FrontPage extends AppCompatActivity {
 

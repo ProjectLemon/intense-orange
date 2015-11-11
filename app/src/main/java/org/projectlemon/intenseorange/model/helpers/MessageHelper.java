33f04c549d51c8764729fe38a9706bcf3740b91f
Jenny Kinert <id14jkt@cs.umu.se>
2015-11-11 16:10:56 +0100

    Add: more files

--- app/src/main/java/org/projectlemon/intenseorange/model/helpers/MessageHelper.java
+++ app/src/main/java/org/projectlemon/intenseorange/model/helpers/MessageHelper.java
@@ -1,7 +1,11 @@
 package org.projectlemon.intenseorange.model.helpers;
 
+import android.net.wifi.p2p.WifiP2pDevice;
+
+import org.projectlemon.intenseorange.model.utilities.clientKey;
 import org.projectlemon.intenseorange.model.wifiPackage.WifiPackage;
 
+import java.util.HashMap;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -12,8 +16,13 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class MessageHelper {
     private Queue<WifiPackage> messages = new LinkedBlockingQueue<>();
+    /*
+        Wifip2pDevice may be switched to client objects. That should be created as a class
+     */
+    private HashMap<clientKey, WifiP2pDevice> connectedPeers = new HashMap<>();
 
     public void addMessageToQueue(WifiPackage packageMessage){
         messages.add(packageMessage);
     }
+
 }
---
+++ app/src/main/java/org/projectlemon/intenseorange/model/utilities/clientKey.java
@@ -0,0 +1,11 @@
+package org.projectlemon.intenseorange.model.utilities;
+
+/**
+ * Created by Jenny on 2015-11-11.
+ */
+public class clientKey {
+
+    clientKey(){
+
+    }
+}

0880e8379329da179b5466b33c003c4071512c81
Jenny Kinert <id14jkt@cs.umu.se>
2015-11-11 15:07:36 +0100

    Add: Networkfiles to repo

---
+++ app/src/main/java/org/projectlemon/intenseorange/model/WifiPackage/WifiPackage.java
@@ -0,0 +1,7 @@
+package org.projectlemon.intenseorange.model.wifiPackage;
+
+/**
+ * Created by Jenny on 2015-11-11.
+ */
+public class WifiPackage {
+}
---
+++ app/src/main/java/org/projectlemon/intenseorange/model/helpers/MessageHelper.java
@@ -0,0 +1,20 @@
+package org.projectlemon.intenseorange.model.helpers;
+
+import org.projectlemon.intenseorange.model.wifiPackage.WifiPackage;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by Jenny on 2015-11-11.
+ * Contain queue with messages to send and a list of peers to send messages to.
+ * As long as the program runs and the queue contains messages it should sen these to every peer.
+ */
+public class MessageHelper {
+    private Queue<WifiPackage> messages = new LinkedBlockingQueue<>();
+
+
+    public void addMessageToQueue(WifiPackage packageMessage){
+        messages.add(packageMessage);
+    }
+}

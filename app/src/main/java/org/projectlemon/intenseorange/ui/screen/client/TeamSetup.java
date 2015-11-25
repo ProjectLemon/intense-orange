8e415e401acdfe1fa49eeba39e2d0bbd4c106ff1
Fredrik Johansson <f95johansson@gmail.com>
2015-11-25 11:02:48 +0100

    Modified: .gitignore to not include build.gradle files

---
+++ app/src/main/java/org/projectlemon/intenseorange/ui/screen/client/TeamSetup.java
@@ -0,0 +1,11 @@
+package org.projectlemon.intenseorange.ui.screen.client;
+
+/**
+ * File: TeamSetup.java
+ * Project: Intense Orange
+ * Author: Fredrik Johansson
+ * Date: 2015.11.25
+ */
+
+public class TeamSetup {
+}

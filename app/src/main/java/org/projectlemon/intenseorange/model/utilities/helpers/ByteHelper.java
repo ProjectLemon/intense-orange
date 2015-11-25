8de01798a0144bf525f9f17ea9fd7f123809b829
Jenny Kinert <id14jkt@cs.umu.se>
2015-11-25 16:58:46 +0100

    Add: network classes Started working on ByteHelper

--- app/src/main/java/org/projectlemon/intenseorange/model/utilities/helpers/ByteHelper.java
+++ app/src/main/java/org/projectlemon/intenseorange/model/utilities/helpers/ByteHelper.java
@@ -1,7 +1,31 @@
 package org.projectlemon.intenseorange.model.utilities.helpers;
 
+import java.util.ArrayList;
+
 /**
+ * Class: ByteHelper
+ * Purpose: Class to build byte sequences for information to be send between units in the peer
+ * network.
  * Created by linuslagerhjelm on 15-11-25.
  */
 public class ByteHelper {
+
+   private ArrayList<Byte> arrayOfBytes = new ArrayList<>();
+
+    public ByteHelper(byte... arrayOfBytes) {
+        addByte(arrayOfBytes);
+    }
+
+    /**
+     * Take bytes as an input parameter and stores it in the arrayList arrayOfBytes.
+     * @param arrayOfBytes
+     * @return
+     */
+
+    public byte[] addByte(byte... arrayOfBytes){
+        for (byte b : arrayOfBytes) {
+            this.arrayOfBytes.add(b);
+        }
+        return arrayOfBytes;
+    }
 }

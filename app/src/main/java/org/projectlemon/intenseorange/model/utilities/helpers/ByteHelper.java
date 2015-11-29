fd43ce51ff2be471b4ac978ec043f3439e88e597
Jenny Kinert <id14jkt@cs.umu.se>
2015-11-29 17:14:01 +0100

    Add: network classes Almost done with ByteHelper.java

--- app/src/main/java/org/projectlemon/intenseorange/model/utilities/helpers/ByteHelper.java
+++ app/src/main/java/org/projectlemon/intenseorange/model/utilities/helpers/ByteHelper.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 public class ByteHelper {
 
    private ArrayList<Byte> arrayOfBytes = new ArrayList<>();
+    private boolean dividedByFour;
 
     public ByteHelper(byte... arrayOfBytes) {
         addByte(arrayOfBytes);
@@ -28,4 +29,63 @@ public class ByteHelper {
         }
         return arrayOfBytes;
     }
+
+    /**
+     * Take a short as input and modifies it to bytes.
+     * @param s
+     * @return
+     */
+    public ArrayList<Byte> addShort(Short s){
+        this.arrayOfBytes.add((byte) ((s >> 8) & 0xff));
+        this.arrayOfBytes.add((byte) (s & 0xff));
+        return arrayOfBytes;
+    }
+
+    public ArrayList<Byte> addInt(int i){
+        this.arrayOfBytes.add((byte) ((i >> 24) & 0xff));
+        this.arrayOfBytes.add((byte) ((i >> 16) & 0xff));
+        this.arrayOfBytes.add((byte) ((i >> 8) & 0xff));
+        this.arrayOfBytes.add((byte) (i & 0xff));
+        return arrayOfBytes;
+    }
+
+    /**
+     * This is a builder. It builds a byte array by the pieces in the arraylist.
+     * @return result;
+     */
+    public byte[] toByteArray() {
+        byte[] result = new byte[arrayOfBytes.size()];
+        for (int i = 0; i < arrayOfBytes.size(); i++) {
+            result[i] = arrayOfBytes.get(i);
+        }
+        return result;
+    }
+
+    /**
+     * Pad the sequence with byte zeros so it's always divisible by four.
+     * @return
+     */
+    public ArrayList<Byte> pad(){
+
+        dividedByFour = false;
+
+        while(!dividedByFour){
+            if(arrayOfBytes.size()%4!=0) {
+                arrayOfBytes.add((byte)0);
+            }
+            else{
+                dividedByFour = true;
+            }
+        }
+        return arrayOfBytes;
+    }
+
+    /**
+     * Returns the size of the array.
+     * @return
+     */
+    public int size() {
+        return arrayOfBytes.size();
+    }
+
 }
